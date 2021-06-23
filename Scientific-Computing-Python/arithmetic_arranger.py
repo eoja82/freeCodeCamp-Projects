9090ca139fedb287ed326f0631551411525836a1
Erik Oja <erik_oja@yahoo.com>
2021-06-23 17:45:34 -0500

    finished pre testing

--- Scientific-Computing-Python/arithmetic_arranger.py
+++ Scientific-Computing-Python/arithmetic_arranger.py
@@ -1,17 +1,16 @@
-def arithmetic_arranger(problems):
+def arithmetic_arranger(problems, *printAnswer):
     if len(problems) > 5:
         return "Error: Too many problems."
-
+    
     def getAnswer(x):
         if x[1] == "+":
             return str(int(x[0]) + int(x[2]))
         else:
             return str(int(x[0]) - int(x[2]))
     
-    maxOperand = 4
     operators = ["+", "-"]
     problems = [x.split(" ") for x in problems]
-    #print(probs)
+    
     for x in problems:
         if len(x[0]) > 4 or len(x[2]) > 4:
             return "Error: Numbers cannot be more than four digits."
@@ -28,14 +27,36 @@ def arithmetic_arranger(problems):
         x.append("".join(dashes))
         x.append(getAnswer(x))
         
-    print(problems)
+    #print(problems)
+    result = ""
 
+    for i in range(4):
+        if i == 0:
+            for x in problems:
+                result = result + (" " * (len(x[3]) - len(x[0]))) + x[0] + "    "
+            result = result + "\n"
+        if i == 1:
+            for x in problems:
+                spaces = len(x[0]) - len(x[2]) if len(x[0]) > len(x[2]) else 0
+                result = result + x[1] + " " + (" " * spaces) + x[2] + "    "
+            result = result + "\n"
+        if i == 2:
+            continue
+        if i == 3:
+            for x in problems:
+                result = result + x[3] + "    "
+            if printAnswer:
+                result = result + "\n"
+                for x in problems:
+                    spaces = len(x[3]) - len(x[4])
+                    result = result + (" " * spaces) + x[4] + "    "
         
-
+    return result
     #return arranged_problems
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
-""" print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49", "1 + 2", "5 + 3"]))
+print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True))
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49", "1 + 2", "5 + 3"]))
 print(arithmetic_arranger(["32 + 698", "38019 - 2999", "45 + 43", "123 + 49"]))
 print(arithmetic_arranger(["32 + 698", "3801 * 2", "45 + 43", "123 + 49"]))
-print(arithmetic_arranger(["32 + 688", "3801 + 2", "45 + J80", "123 + 49"])) """
+print(arithmetic_arranger(["32 + 688", "3801 + 2", "45 + J80", "123 + 49"]))
