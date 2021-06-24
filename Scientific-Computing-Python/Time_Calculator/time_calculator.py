06a2bf40eafad1fd99b5bd3a71e91de1c0955696
Erik Oja <erik_oja@yahoo.com>
2021-06-24 18:07:47 -0500

    passing all tests

--- Scientific-Computing-Python/Time_Calculator/time_calculator.py
+++ Scientific-Computing-Python/Time_Calculator/time_calculator.py
@@ -5,11 +5,9 @@ def add_time(start, duration, *day):
     start = re.split(":|\s", start)
     start[0] = int(start[0])
     start[1] = int(start[1])
-    #print(start)
     duration = duration.split(":")
     duration[0] = int(duration[0])
     duration[1] = int(duration[1])
-    #print(duration)
     hours = start[0] + duration[0]
     if start[2] == "PM":
         hours += 12
@@ -19,22 +17,45 @@ def add_time(start, duration, *day):
         minutes = minutes - 60
     minutes = "0" + str(minutes) if minutes < 10 else str(minutes)
     ampm = "AM"
-    days = math.floor(hours / 24)
+    daysLater = math.floor(hours / 24)
     hours = hours % 24
     if hours == 0:
         hours = "12"
+    elif hours == 12:
+        ampm = "PM"
+        hours = str(hours)
     elif hours > 12:
         ampm = "PM"
         hours = str(hours - 12)
     else:
         hours = str(hours)
-    
-    print(hours, minutes, ampm, days)
+
+    dayNames = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+    newDay = ""
+    if day:
+        day = day[0].lower()
+        index = dayNames.index(day)
+        newDayIndex = index + daysLater % 7
+        if newDayIndex > 6:
+            newDayIndex -= 7
+        newDay = dayNames[newDayIndex]
+        newDay = newDay.capitalize()
+
+    result = hours + ":" + minutes + " " + ampm
+    if day:
+        result = result + ", " + newDay
+    if daysLater > 0:
+        if daysLater == 1:
+            result = result + " (next day)"
+        else:
+            result = result + " (" + str(daysLater) + " days later)"
+
+    return result
 
 # for development testing
-add_time("3:00 PM", "3:10")              # Returns: 6:10 PM
-add_time("11:30 AM", "2:32", "Monday")   # Returns: 2:02 PM, Monday
-add_time("11:43 AM", "00:20")            # Returns: 12:03 PM
-add_time("10:10 PM", "3:30")             # Returns: 1:40 AM (next day)
-add_time("11:43 PM", "24:20", "tueSday") # Returns: 12:03 AM, Thursday (2 days later)
-add_time("6:30 PM", "205:12")            # Returns: 7:42 AM (9 days later)
+""" print(add_time("3:00 PM", "3:10"))              # Returns: 6:10 PM
+print(add_time("11:30 AM", "2:32", "Monday"))   # Returns: 2:02 PM, Monday
+print(add_time("11:43 AM", "00:20"))            # Returns: 12:03 PM
+print(add_time("10:10 PM", "3:30"))             # Returns: 1:40 AM (next day)
+print(add_time("11:43 PM", "24:20", "tueSday")) # Returns: 12:03 AM, Thursday (2 days later)
+print(add_time("6:30 PM", "205:12"))            # Returns: 7:42 AM (9 days later) """
