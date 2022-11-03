b04ffc1963142fd4f257a3c885ee10ac04ce480f
Erik Oja <erik_oja@yahoo.com>
2022-11-03 16:30:21 -0500

    passing all tests

---
+++ Relational-Database/Build-a-World-Cup-Database/insert_data.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+echo $($PSQL "TRUNCATE teams, games;")
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+
+do
+  # skip first line
+  if [[ $YEAR != year ]]
+
+  # insert into teams
+  then
+    # get team ids
+    WIN_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER'")
+    OPP_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT'")
+
+    # insert team if not found
+    if [[ -z $WIN_ID ]]
+    then
+      RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      if [[ $RESULT = 'INSERT 0 1' ]]
+        then
+          echo Inserted $WINNER
+      fi
+    fi
+
+    if [[ -z $OPP_ID ]]
+    then
+      RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      if [[ $RESULT = 'INSERT 0 1' ]]
+        then
+          echo Inserted $OPPONENT
+      fi
+    fi
+
+  fi
+
+done
+
+echo new read
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  # get team ids
+    WIN_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER'")
+    OPP_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT'")
+
+    echo $WIN_ID $OPP_ID
+
+    # insert into games
+    GAMES=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WIN_ID, $OPP_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+    echo $GAMES
+
+done
