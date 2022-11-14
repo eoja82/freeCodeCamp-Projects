e9f155503d6d187da967d2b6e29844278f3ad0fd
Erik Oja <erik_oja@yahoo.com>
2022-11-14 16:39:34 -0600

    passing all tests

--- Relational-Database/Build-a-Salon-Appointment-Scheduler/salon.sh
+++ Relational-Database/Build-a-Salon-Appointment-Scheduler/salon.sh
@@ -20,6 +20,68 @@ MAIN_MENU() {
   done
   echo -e "x) exit\n"
 
+  # get service
+  read SERVICE_ID_SELECTED
+
+  # if x case insensitve exit
+  if [[ $SERVICE_REQUESTED =~ ^[x|X]$ ]]
+  then
+    EXIT
+  else
+    # check if SERVICE_ID is a number
+    if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
+    
+    # if not a number send to MAIN_MENU
+    then
+      MAIN_MENU "I could not find that service. What would you like today?"
+    
+    # check if number entered is a service
+    else
+      SERVICE_SELECTED=$($PSQL "SELECT name FROM services WHERE service_id = '$SERVICE_ID_SELECTED'")
+
+      # if not a service send back to MAIN_MENU
+      if [[ -z $SERVICE_SELECTED ]]
+      then
+        MAIN_MENU "I could not find that service. What would you like today?"
+      else
+        # get customer info
+        echo -e "\nWhat's your phone number?"
+        read CUSTOMER_PHONE
+
+        CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+        # if customer doesn't exist
+        if [[ -z $CUSTOMER_NAME ]]
+        then
+          # get new customer name
+          echo -e "\nI don't have a record for that phone number, what's your name?"
+          read CUSTOMER_NAME
+
+          # insert new customer
+          INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')") 
+        fi
+
+        # get customer_id
+        CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+        # get appointment time
+        SERVICE_SELECTED_FORMATTED=$(echo $SERVICE_SELECTED | sed 's/ |/"/')
+        CUSTOMER_NAME_FORMATED=$(echo $CUSTOMER_NAME | sed 's/ |/"/')
+
+        echo -e "\nWhat time would you like your $SERVICE_SELECTED_FORMATTED, $CUSTOMER_NAME_FORMATED?"
+        read SERVICE_TIME
+
+        INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+
+        echo -e "\nI have put you down for a $SERVICE_SELECTED_FORMATTED at $SERVICE_TIME, $CUSTOMER_NAME_FORMATED."
+
+      fi
+
+    fi
+
+  fi
+
+
 }
 
 
